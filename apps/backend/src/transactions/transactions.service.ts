a88d8b3c02f66b76dc98c72b5c41ec40bdac13c3
akiraacs <alfabeto.coradini@hotmail.com>
2026-03-16 23:41:46 -0300

    Cria camada de service para transactions

--- apps/backend/src/transactions/transactions.service.ts
+++ apps/backend/src/transactions/transactions.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service.js';
 
 @Injectable()
-export class TransactionsService {}
+export class TransactionsService {
+    constructor(private prisma: PrismaService) { }
+
+    async findAll(userId: string) {
+        return this.prisma.transaction.findMany({
+            where: { userId },
+            orderBy: { date: 'desc' },
+        });
+    }
+
+    async create(data: {
+        userId: string;
+        type: string;
+        amount: number;
+        category: string;
+        description?: string;
+        date: Date;
+    }) {
+        return this.prisma.transaction.create({ data })
+    }
+
+    async delete(id: string, userId: string) {
+        return this.prisma.transaction.delete({
+            where: { id, userId },
+        });
+    }
+}
