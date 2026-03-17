a90fd209752d58b77ce016988503acb46a9ae4fe
akiraacs <alfabeto.coradini@hotmail.com>
2026-03-16 23:43:57 -0300

    Adiciona swagger a aplicacao

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Finance Tracker API')
+    .setDescription('API do finance tracker')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document)
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
