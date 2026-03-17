4c4102842c24a76b3ba24895b098ea9ad32c2940
akiraacs <alfabeto.coradini@hotmail.com>
2026-03-16 23:40:53 -0300

    Adiciona controller para transactions

--- apps/backend/src/transactions/transactions.controller.ts
+++ apps/backend/src/transactions/transactions.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
+import { CreateTransactionDto } from './dto/create-transaction.dto.js';
+import { TransactionsService } from './transactions.service.js';
 
 @Controller('transactions')
-export class TransactionsController {}
+export class TransactionsController {
+    constructor(private service: TransactionsService) { }
+
+    @Get()
+    findAll(@Query('userId') userId: string) {
+        return this.service.findAll(userId)
+    }
+
+    @Post()
+    create(@Body() body: CreateTransactionDto) {
+        return this.service.create({
+            ...body,
+            date: new Date(body.date),
+        })
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string, @Query('userId') userId: string) {
+        return this.service.delete(id, userId)
+    }
+}
